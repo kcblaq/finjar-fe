453f6ef3100c0d81a10946211311a87d45369f24
Kelechi Ugwu <kcblack22@gmail.com>
2024-12-22 02:51:40 +0100

    Next setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <div className="grid items-center justify-center w-full h-screen ">
+     <h2 className="text-4xl font-bold text-center">Welcome to Next.js</h2>
+
+    </div>
+  );
+}
